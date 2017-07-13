9e1e9757d9a748f4fe411c559f24e154c3c775cc
Joe Dougherty <joseph.dougherty@gmail.com>
2017-07-13 01:33:58 -0500

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+        name='sentential',
+        version='0.0.1',
+        description="A sentential logic interpreter/evaluator.",
+        author_email='joseph.dougherty@gmail.com',
+        packages=['sentential'],
+        install_requires=['prettytable'],
+        package_data={
+            '': ['LICENSE', 'NOTICE', '*.rst'],
+            },
+        classifiers=(
+           'Programming Language :: Python',
+           'Programming Language :: Python :: 2.7',
+           'Programming Language :: Python :: 3',
+           'Programming Language :: Python :: 3.5',
+           'Development Status :: 4 - Beta',
+           'Environment :: Console',
+           'Intended Audience :: End Users/Desktop',
+           'Intended Audience :: Education',
+           'Intended Audience :: Science/Research',
+        ),
+)
