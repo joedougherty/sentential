fdfa04ce8a82cef07b7020627b5a293f37b4046a
joe <joseph.dougherty@gmail.com>
2017-07-20 00:11:42 -0400

    Adds some more utils tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -15,3 +15,25 @@ def test_flat_list():
 
 def test_simple_nested_list():
     assert list_is_nested([True, or_, [True, and_, False]]) == True
+
+
+def test_flat_pop_left_innermost():
+    # If the given AST is a flat list, return the list
+    mock_ast = [True, and_, False]
+    assert pop_left_innermost(mock_ast) == [True, and_, False]
+
+
+def test_nested_pop_left_innermost_return_value():
+    """ Test that the correct sub-list is returned """
+    mock_ast = [True, and_, False, [True, or_, False], [1, 2, 3]]
+    assert pop_left_innermost(mock_ast) == [True, or_, False]
+    
+
+def test_nested_pop_left_innermost_list_is_updated():
+    """
+    Test that the list is altered appropriately
+    The returned sub-list should no longer be present in the original list 
+    """
+    mock_ast = [True, and_, False, [True, or_, False], [1, 2, 3]]
+    pop_left_innermost(mock_ast)
+    assert mock_ast == [True, and_, False, [1, 2, 3]]
