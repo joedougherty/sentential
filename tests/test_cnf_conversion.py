89ee157bd069513b71a7502501e6ef9f6333c5b8
joe <joseph.dougherty@gmail.com>
2017-09-03 18:04:03 -0400

    Adds simpler cnf truth table test

--- tests/test_cnf_conversion.py
+++ tests/test_cnf_conversion.py
@@ -3,6 +3,13 @@ from sentential import Proposition
 from sentential.rewrite_rules import cnf
 from sentential.Expression import expressify
 
+
+def test_simple_prop_cnf_has_same_truth_table():
+    pq = Proposition('''p -> q''')
+    pq_cnf = Proposition(str(cnf(expressify(pq))))
+    assert same_truth_table(pq, pq_cnf)
+
+
 def test_complex_prop_cnf_has_same_truth_table():
     pqrst = Proposition('''((p v q) -> (r -> s)) <-> ~t''')
     pqrst_cnf = Proposition(str(cnf(expressify(pqrst))))
