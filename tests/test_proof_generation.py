fab3a5127d65977ec31df710c8c3eba4f4cbb2d7
joe <joseph.dougherty@gmail.com>
2017-09-17 15:31:02 -0400

    Adds new full proof test

--- tests/test_proof_generation.py
+++ tests/test_proof_generation.py
@@ -1,5 +1,8 @@
 from sentential import Proposition
+from sentential.Expression import expressify
 from sentential.KnowledgeBase import KnowledgeBase 
+from sentential.Proof import Proof
+from sentential.rewrite_rules import group_cnf, cnf
 
 pq = Proposition('''p v q''')
 pr = Proposition('''p -> r''')
@@ -18,3 +21,20 @@ def test_minimal_kb_proof():
     kb.add_goal(r)
     conclusion = kb.prove()
     assert conclusion == True
+
+def test_unsatisfiable_clause_collection():
+    ''' http://intrologic.stanford.edu/exercises/exercise_05_03.html '''
+    kb = KnowledgeBase()
+
+    goal_as_prop = Proposition('''p v q''')
+    goal = group_cnf(cnf(expressify(goal_as_prop)))
+
+    negated_goal_as_prop = Proposition('''!(p v q)''')
+    negated_goal = group_cnf(cnf(expressify(negated_goal_as_prop)))
+
+    c1, c2, c3, c4 = goal[0], {'~p', 'r'}, {'~p', '~r'}, {'p', '~q'}
+
+    goal = c1
+    unsatisfiable = Proof(goal, negated_goal, clause_collection=[c1, c2, c3, c4])
+    # If a contradiction (empty clause) is found, the set of clauses is unsatisfiable
+    assert unsatisfiable.find() == True
