38ab1c1cf393bb39d19bc7508c7a4a84c9144502
Joe Dougherty <joseph.dougherty@gmail.com>
2017-09-19 10:56:25 -0400

    Adds LNC/LEM tests

--- tests/test_proof_generation.py
+++ tests/test_proof_generation.py
@@ -1,6 +1,6 @@
 from sentential import Proposition
 from sentential.Expression import expressify
-from sentential.KnowledgeBase import KnowledgeBase 
+from sentential.KnowledgeBase import KnowledgeBase
 from sentential.Proof import Proof
 from sentential.rewrite_rules import group_cnf, cnf
 
@@ -22,8 +22,6 @@ def test_minimal_kb_proof():
 
 def test_unsatisfiable_clause_collection():
     ''' http://intrologic.stanford.edu/exercises/exercise_05_03.html '''
-    kb = KnowledgeBase()
-
     goal_as_prop = Proposition('''p v q''')
     goal = group_cnf(cnf(expressify(goal_as_prop)))
 
@@ -40,7 +38,7 @@ def test_unsatisfiable_clause_collection():
 def test_multiple_goal_clauses():
     kb = KnowledgeBase()
 
-    kb.add_axiom(Proposition('''p -> q''')) 
+    kb.add_axiom(Proposition('''p -> q'''))
     kb.add_axiom(Proposition('''r -> s'''))
     kb.add_goal(Proposition('''(p v r) -> (q v s)'''))
 
@@ -49,8 +47,8 @@ def test_multiple_goal_clauses():
 def test_another_multiple_goal_clauses():
     kb = KnowledgeBase()
 
-    kb.add_axiom(Proposition('''(p -> q) -> q'''))         
-    kb.add_axiom(Proposition('''(p -> p) -> r'''))   
+    kb.add_axiom(Proposition('''(p -> q) -> q'''))
+    kb.add_axiom(Proposition('''(p -> p) -> r'''))
     kb.add_axiom(Proposition('''(r -> s) -> ~(s -> q)'''))
     kb.add_goal(Proposition('''r'''))
 
@@ -100,7 +98,7 @@ def test_hypothetical_syllogism():
     axioms = ['a -> b', 'b -> c', 'c -> d', 'd -> e', 'e -> f', 'f -> g', 'a']
     [kb.add_axiom(Proposition(statement)) for statement in axioms]
     kb.add_goal(Proposition('''g'''))
-    
+
     assert kb.prove() == True
 
 def test_affirming_the_consequent():
@@ -113,7 +111,7 @@ def test_affirming_the_consequent():
 
     assert kb.prove() == False
 
-def test_construtive_dilemma():
+def test_constructive_dilemma():
     '''
     It is the inference that, if P implies Q and R implies S and either P or R is true, then Q or S has to be true.
     Source: https://en.wikipedia.org/wiki/Constructive_dilemma
@@ -158,5 +156,47 @@ def test_exercise_5_4():
     kb.add_axiom(Proposition('''p -> q'''))
     kb.add_axiom(Proposition('''r -> s'''))
     kb.add_goal(Proposition('''(p v r) -> (q v s)'''))
+
+    assert kb.prove() == True
+
+def test_lnc_is_theorem():
+    kb = KnowledgeBase()
+
+    lnc = Proposition('''~(p & ~p)''')
+
+    kb.add_axiom(lnc)
+    kb.add_goal(lnc)
+
+    assert kb.prove() == True
+
+def test_lem_is_theorem():
+    kb = KnowledgeBase()
+
+    lem = Proposition('''p v ~p''')
+    kb.add_axiom(lem)
+    kb.add_goal(lem)
+
+    assert kb.prove() == True
+
+def test_lnc_conflicts_with_negated_lnc():
+    """ You should *not* be able to derive a negation of a theorem (from itself)! """
+    kb = KnowledgeBase()
+
+    lnc = Proposition('''~(p & ~p)''')
+    negated_lnc = Proposition('''~(~(p & ~p))''')
+
+    kb.add_axiom(lnc)
+    kb.add_goal(negated_lnc)
+
+    assert kb.prove() == False
+
+def test_lem_and_lnc_imply_one_another():
+    kb = KnowledgeBase()
+
+    lem = Proposition('''p v ~p''')
+    lnc = Proposition('''~(p & ~p)''')
+
+    kb.add_axiom(lem)
+    kb.add_goal(lnc)
 
     assert kb.prove() == True
