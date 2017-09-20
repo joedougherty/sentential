7f76070d6e0f47e97d545ee4903448928074bbfc
joe <joseph.dougherty@gmail.com>
2017-09-20 18:07:53 -0400

    Adds tests to demonstrate that a number of proofs return false if they are unsatisfiable

--- tests/test_rules_of_inference_unsatisfiable.py
+++ tests/test_rules_of_inference_unsatisfiable.py
@@ -7,15 +7,49 @@ from sentential.Proof import Proof
 from sentential.rewrite_rules import group_cnf, cnf
 
 ''' 
-Test known rules of inference
-Source: https://en.wikipedia.org/wiki/List_of_rules_of_inference
+Think of this as the complement to test_rules_of_inference.
+
+Where test_rules_of_inference verifies that a set of axioms
+can produce a proof for a known rule of inference, 
+test_rules_of_inference_unsatisfiable tests cases that *should not*
+be provable.
 '''
 
-def test_biconditional_elimination_unsatisifiable_unrelated_prop():
+def test_case_analysis_unsat():
     kb = KnowledgeBase()
 
-    axioms = ['j <-> r', '~r v ~j']
+    axioms = ['a -> b', 'c -> b', 'a v c']
     [kb.add_axiom(Proposition(statement)) for statement in axioms]
-    kb.add_goal(Proposition('''l'''))
+    kb.add_goal(Proposition('''~b'''))
+
+    assert kb.prove() == False
+
+
+def test_disjunctive_syllogism_unsat_v1():
+    kb = KnowledgeBase()
+
+    axioms = ['p v q', '~p']
+    [kb.add_axiom(Proposition(statement)) for statement in axioms]
+    kb.add_goal(Proposition('''~q'''))
+
+    assert kb.prove() == False
+
+
+def test_disjunctive_syllogism_unsat_v2():
+    kb = KnowledgeBase()
+
+    axioms = ['p v q', '~q']
+    [kb.add_axiom(Proposition(statement)) for statement in axioms]
+    kb.add_goal(Proposition('''~p'''))
+
+    assert kb.prove() == False
+
+
+def test_constructive_dilemma():
+    kb = KnowledgeBase()
+
+    axioms = ['a -> b', 'c -> d', 'a v c']
+    [kb.add_axiom(Proposition(statement)) for statement in axioms]
+    kb.add_goal(Proposition('''~(b v d)'''))
 
     assert kb.prove() == False
